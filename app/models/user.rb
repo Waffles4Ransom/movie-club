8e3981c27f7a7de374b87feefc97130744c2ac0e
Jenna Ransom <jenna.ransom@gmail.com>
2020-03-01 00:55:53 -0700

    change sign up form, add validations, and quick show page test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   has_many :comments 
   has_many :commented_reviews, through: :comments, source: :review
 
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
