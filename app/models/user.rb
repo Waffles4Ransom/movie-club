523557edf07a22e36eeb24996b048d297f7d8e7f
Jenna Ransom <jenna.ransom@gmail.com>
2020-02-29 23:27:59 -0700

    add user sign up route, action and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :reviews
   has_many :movies, through: :reviews
   has_many :comments 
   has_many :commented_reviews, through: :comments, source: :review
+
+  validates :username, uniqueness: true
 end
