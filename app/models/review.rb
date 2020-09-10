0b722e02fef887df9c8e37eb18bcba0ca267912e
Jenna Ransom <jenna.ransom@gmail.com>
2020-09-09 18:56:02 -0600

    squashing bugs

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :movie
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :commenters, through: :comments, source: :user
 
   validates :rating, :content, presence: true 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_one :profile, dependent: :destroy
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :movies, through: :reviews
   has_many :comments 
   has_many :commented_reviews, through: :comments, source: :review
